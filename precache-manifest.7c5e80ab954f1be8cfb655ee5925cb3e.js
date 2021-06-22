8ba8a75ee7610755743030746652be1f84cf4089
shauandsss <shauands@gmail.com>
2021-06-21 21:31:53 -0300

    Updates

---
+++ precache-manifest.7c5e80ab954f1be8cfb655ee5925cb3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ce922c2bd6989869fa2",
+    "url": "/lart/static/css/main.66667ef5.chunk.css"
+  },
+  {
+    "revision": "8ce922c2bd6989869fa2",
+    "url": "/lart/static/js/main.40fe24ba.chunk.js"
+  },
+  {
+    "revision": "0311d2b2d7ea713e1adc",
+    "url": "/lart/static/js/runtime~main.7198c191.js"
+  },
+  {
+    "revision": "ee30108de6c9328eab49",
+    "url": "/lart/static/js/2.dfc1f34d.chunk.js"
+  },
+  {
+    "revision": "88fafd8a6ef972df2ba236405990996b",
+    "url": "/lart/index.html"
+  }
+];
